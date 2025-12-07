1a894323f4c223f8506b4f1b5f29b4e00a4268bc
Tobias Kolb <tkolb80@gmx.net>
2025-12-07 20:22:00 +0100

    Implement Base52 encoder

--- TextEncoder/Encoder/Base52Encoder.cs
+++ TextEncoder/Encoder/Base52Encoder.cs
@@ -1,31 +1,18 @@
 namespace TextEncoder.Encoder;
 
-/// <summary>
-/// Like Base64 but with a limited character set.
-/// </summary>
-public class Base52Encoder : Base64Encoder
+public class Base52Encoder : BaseEncoderWithCustomCharset
 {
-	/// <summary>
-	/// Only includes upper and lower case characters of the alphabet.
-	/// </summary>
-	public static readonly Base52Encoder WithoutDigits = new Base52Encoder(Base52Mapping.WithoutDigits);
+    /// <summary>
+    /// Only includes upper and lower case characters of the alphabet.
+    /// </summary>
+    public static readonly Base52Encoder WithoutDigits = new Base52Encoder(Base52Mapping.WithoutDigits);
 
-	/// <summary>
-	/// Includes numerical digits but excludes vowels
-	/// </summary>
-	public static readonly Base52Encoder WithoutVowels = new Base52Encoder(Base52Mapping.WithoutVowels);
+    /// <summary>
+    /// Includes numerical digits but excludes vowels
+    /// </summary>
+    public static readonly Base52Encoder WithoutVowels = new Base52Encoder(Base52Mapping.WithoutVowels);
 
-	private class Base52Mapping : CharacterMapping
-	{
-		internal static readonly Base52Mapping WithoutDigits = new("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
-		internal static readonly Base52Mapping WithoutVowels = new("0123456789BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz");
-
-		private Base52Mapping(string characterSet) : base(characterSet.ToCharArray(), '=')
-		{
-		}
-	}
-
-	private Base52Encoder(Base52Mapping mapping) : base(mapping)
-	{
-	}
+    private Base52Encoder(Base52Mapping characterMapping) : base(characterMapping)
+    {
+    }
 }
