8ae29f2cbba95c461cc4ff05cf9b171d7a56e958
Tobias Kolb <tobster-de@users.noreply.github.com>
2025-12-08 16:49:48 +0100

    Set up CI with Azure Pipelines (#1)

---
+++ TextEncoderTests/RunTestsInCI.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TextEncoderTests;
+
+[TestFixture]
+public class RunTestsInCI
+{
+    [Test]
+    public void RunInCI()
+    {
+        Assert.Pass();
+    }
+
+}
