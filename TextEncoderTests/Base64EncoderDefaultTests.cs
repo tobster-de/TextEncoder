ccbea979257cafa8507e737b7aeaaaa2e2536277
Tobias Kolb <tkolb80@gmx.net>
2025-12-08 13:00:24 +0100

    Minor maintenance

--- TextEncoderTests/Base64EncoderDefaultTests.cs
+++ TextEncoderTests/Base64EncoderDefaultTests.cs
@@ -3,48 +3,47 @@ using System.Collections;
 using NUnit.Framework;
 using TextEncoder.Encoder;
 
-namespace TextEncoderTests
+namespace TextEncoderTests;
+
+[TestFixture]
+public class Base64EncoderDefaultTests : GenericTestBase<Base64Encoder>
 {
-	[TestFixture]
-	public class Base64EncoderDefaultTests : GenericTestBase<Base64Encoder>
+	public Base64EncoderDefaultTests()
 	{
-		public Base64EncoderDefaultTests()
-		{
-			this.Subject = Base64Encoder.Default;
-		}
+		this.Subject = Base64Encoder.Default;
+	}
 
-		public static IEnumerable TestStrings
+	public static IEnumerable TestStrings
+	{
+		get
 		{
-			get
-			{
-				for (int i = 0; i < Count; i++) yield return new TestCaseData(Convert.ToBase64String(RandomData[i]));
-			}
+			for (int i = 0; i < Count; i++) yield return new TestCaseData(Convert.ToBase64String(RandomData[i]));
 		}
+	}
 
-		public static IEnumerable TestData
+	public static IEnumerable TestData
+	{
+		get
 		{
-			get
-			{
-				for (int i = 0; i < Count; i++) yield return new TestCaseData(RandomData[i]);
-			}
+			for (int i = 0; i < Count; i++) yield return new TestCaseData(RandomData[i]);
 		}
+	}
 
-		[Test, TestCaseSource(nameof(TestData))]
-		public void RoundTrip(byte[] d)
-		{
-			CollectionAssert.AreEqual(d, this.Subject!.FromBase(this.Subject!.ToBase(d)));
-		}
+	[Test, TestCaseSource(nameof(TestData))]
+	public void RoundTrip(byte[] d)
+	{
+		CollectionAssert.AreEqual(d, this.Subject!.FromBase(this.Subject!.ToBase(d)));
+	}
 
-		[Test, TestCaseSource(nameof(TestData))]
-		public void ToBase64(byte[] d)
-		{
-			Assert.AreEqual(Convert.ToBase64String(d), this.Subject!.ToBase(d));
-		}
+	[Test, TestCaseSource(nameof(TestData))]
+	public void ToBase64(byte[] d)
+	{
+		Assert.AreEqual(Convert.ToBase64String(d), this.Subject!.ToBase(d));
+	}
 
-		[Test, TestCaseSource(nameof(TestStrings))]
-		public void FromBase64(string s)
-		{
-			CollectionAssert.AreEqual(Convert.FromBase64String(s), this.Subject!.FromBase(s));
-		}
+	[Test, TestCaseSource(nameof(TestStrings))]
+	public void FromBase64(string s)
+	{
+		CollectionAssert.AreEqual(Convert.FromBase64String(s), this.Subject!.FromBase(s));
 	}
 }
--- TextEncoderTests/Base64EncoderNoPaddingTests.cs
+++ TextEncoderTests/Base64EncoderNoPaddingTests.cs
@@ -3,48 +3,47 @@ using System.Collections;
 using NUnit.Framework;
 using TextEncoder.Encoder;
 
-namespace TextEncoderTests
+namespace TextEncoderTests;
+
+[TestFixture]
+public class Base64EncoderNoPaddingTests : GenericTestBase<Base64Encoder>
 {
-	[TestFixture]
-	public class Base64EncoderNoPaddingTests : GenericTestBase<Base64Encoder>
+	public Base64EncoderNoPaddingTests()
 	{
-		public Base64EncoderNoPaddingTests()
-		{
-			this.Subject = Base64Encoder.DefaultNoPadding;
-		}
+		this.Subject = Base64Encoder.DefaultNoPadding;
+	}
 
-		public static IEnumerable TestStrings
+	public static IEnumerable TestStrings
+	{
+		get
 		{
-			get
-			{
-				for (int i = 0; i < Count; i++) yield return new TestCaseData(Convert.ToBase64String(RandomData[i]));
-			}
+			for (int i = 0; i < Count; i++) yield return new TestCaseData(Convert.ToBase64String(RandomData[i]));
 		}
+	}
 
-		public static IEnumerable TestData
+	public static IEnumerable TestData
+	{
+		get
 		{
-			get
-			{
-				for (int i = 0; i < Count; i++) yield return new TestCaseData(RandomData[i]);
-			}
+			for (int i = 0; i < Count; i++) yield return new TestCaseData(RandomData[i]);
 		}
+	}
 
-		[Test, TestCaseSource(nameof(TestData))]
-		public void RoundTrip(byte[] d)
-		{
-			CollectionAssert.AreEqual(d, this.Subject!.FromBase(this.Subject!.ToBase(d)));
-		}
+	[Test, TestCaseSource(nameof(TestData))]
+	public void RoundTrip(byte[] d)
+	{
+		CollectionAssert.AreEqual(d, this.Subject!.FromBase(this.Subject!.ToBase(d)));
+	}
 
-		[Test, TestCaseSource(nameof(TestData))]
-		public void ToBase64(byte[] d)
-		{
-			Assert.That(Convert.ToBase64String(d).StartsWith(this.Subject!.ToBase(d)));
-		}
+	[Test, TestCaseSource(nameof(TestData))]
+	public void ToBase64(byte[] d)
+	{
+		Assert.That(Convert.ToBase64String(d).StartsWith(this.Subject!.ToBase(d)));
+	}
 
-		[Test, TestCaseSource(nameof(TestStrings))]
-		public void FromBase64(string s)
-		{
-			CollectionAssert.AreEqual(Convert.FromBase64String(s), this.Subject!.FromBase(s.Trim('=')));
-		}
+	[Test, TestCaseSource(nameof(TestStrings))]
+	public void FromBase64(string s)
+	{
+		CollectionAssert.AreEqual(Convert.FromBase64String(s), this.Subject!.FromBase(s.Trim('=')));
 	}
 }
